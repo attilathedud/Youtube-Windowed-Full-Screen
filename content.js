c06407c5c3ec4095dd93854907e9cedae9fce427
attilathedud <attilathedud@gmail.com>
2020-09-26 11:32:39 -0500

    Add files via upload

---
+++ content.js
@@ -0,0 +1,6 @@
+document.getElementById('masthead-container').style.display = 'none';
+document.getElementById('page-manager').style.marginTop = '0px';
+document.getElementById('player-theater-container').style.maxHeight = '100vh';
+document.getElementsByTagName('video')[0].style.left = '0px';
+document.getElementsByTagName('video')[0].style.height = '100vh';
+document.getElementsByTagName('video')[0].style.width = '100%';
